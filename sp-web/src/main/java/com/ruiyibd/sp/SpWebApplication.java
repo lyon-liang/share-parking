466ce44444405bf4954b46096d435dad887146b0
lyon-liang <lfcxgjh375526@163.com>
2021-11-18 23:11:46 +0800

    第一次代码提交

---
+++ sp-web/src/main/java/com/ruiyibd/sp/SpWebApplication.java
@@ -0,0 +1,13 @@
+package com.ruiyibd.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpWebApplication.class, args);
+    }
+
+}
